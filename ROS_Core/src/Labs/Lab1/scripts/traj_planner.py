a5100233d25494af83d7633b3a6c0ba55be16ac2
Christopher Paucar <cpaucar@princeton.edu>
2023-03-03 00:54:22 -0500

    Task 2 done, except pi,-pi, and Task 3 almost done

--- ROS_Core/src/Labs/Lab1/scripts/traj_planner.py
+++ ROS_Core/src/Labs/Lab1/scripts/traj_planner.py
@@ -443,7 +443,45 @@ class TrajectoryPlanner():
                 - Publish the new policy for RVIZ visualization
                     for example: self.trajectory_pub.publish(new_policy.to_msg())       
             '''
-            
+            # take current time and subtract from last replan time
+            curr_time = rospy.get_rostime().to_sec()
+            t_since_replan = curr_time - t_last_replan
+            # Step 1
+            if self.plan_state_buffer.new_data_available and 
+            (t_since_replan > self.replan_dt) and 
+            self.planner_ready:
+                # Step 2
+                curr_state = self.plan_state_buffer.readFromRT()
+                prev_policy = self.policy_buffer.readFromRT()
+                if prev_policy not None:
+                    curr_controls = prev_policy.get_ref_controls(t_last_replan)
+                if self.path_buffer.new_data_available:
+                    updated_path = self.path_buffer.readFromRT()
+                    reference_path = self.planner.update_ref_path(updated_path)
+                    ilqr_obj = ILQR()
+                    ilqr_dict = ilqr_obj.plan(curr_state,curr_controls)
+                    # Step 3
+                    if ilqr_dict['status'] == 0:
+                        new_policy = Policy(curr_state,
+                                            curr_controls,
+                                            ilqr_obj.backward_pass[0],
+                                            curr_time,
+                                            self.planner.dt,
+                                            curr_controls.shape[1]
+                                            )
+
+                        
+                #
+
+                
+
+                
+                
+
+
+
+
+
             ###############################
             #### END OF TODO #############
             ###############################
